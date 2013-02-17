bf6600eda72a957e592859f71912faa6c292ef5c
i09158knct <i09158knct@gmail.com>
2013-02-17 18:54:01 +0900

    add binarization2

--- js/utils.js
+++ js/utils.js
@@ -35,3 +35,22 @@ function dataToNeagative(data) {
     data[bIndex] = 255 - data[bIndex];
   }
 }
+
+function dataToGrayscale(data) {
+  var length = data.length;
+  for (var i = 0; i < length; i += 4) {
+    var rIndex = i + 0;
+    var gIndex = i + 1;
+    var bIndex = i + 2;
+
+    var ntscAverage = (
+      305 * data[rIndex] +
+      601 * data[gIndex] +
+      117 * data[bIndex]
+    ) >> 10;
+
+    data[rIndex] = ntscAverage;
+    data[gIndex] = ntscAverage;
+    data[bIndex] = ntscAverage;
+  }
+}
