f87a89cf872519953b43ab07bf0dc38de7d66c02
i09158knct <i09158knct@gmail.com>
2013-02-19 03:47:03 +0900

    add grouping

--- js/utils.js
+++ js/utils.js
@@ -54,3 +54,117 @@ function dataToGrayscale(data) {
     data[bIndex] = ntscAverage;
   }
 }
+
+function buildGroup(imageData) {
+  var
+    height = imageData.height,
+    width = imageData.width,
+    data = imageData.data,
+    groupTable = [],
+    synonymousIdTable = [],
+    nextMarkId = 1; // id should not use 0
+
+
+  for (var iy = 0, offsetY = 0; iy < height; iy++, offsetY += 4 * width) {
+    var currentRow = groupTable[iy] = [];
+    var aboveRow = groupTable[iy - 1] || [];
+
+    // processing of left end pixel
+    if (data[offsetY + 0] === 0) {
+      currentRow[0] = aboveRow[0] || nextMarkId++;
+    }
+
+    for (var ix = 1, offsetX = 4; ix < width; ix++, offsetX += 4) {
+      if (data[offsetY + offsetX] === 0) {
+        var abovePixelId = aboveRow[ix];
+        var beforePixelId = currentRow[ix - 1];
+        if (abovePixelId) {
+          currentRow[ix] = abovePixelId;
+          if (beforePixelId && beforePixelId !== abovePixelId) {
+            synonymousIdTable[beforePixelId] =
+              synonymousIdTable[beforePixelId] || [];
+            synonymousIdTable[abovePixelId] =
+              synonymousIdTable[abovePixelId] || [];
+            synonymousIdTable[beforePixelId][abovePixelId] = abovePixelId;
+            synonymousIdTable[abovePixelId][beforePixelId] = beforePixelId;
+          }
+        } else {
+          currentRow[ix] = beforePixelId || nextMarkId++;
+        }
+      }
+    }
+  }
+
+  var lookupTable = (function _buildSynonymousLookupTable() {
+    var lookupTable = [];
+    var visitedTable = [];
+    synonymousIdTable.forEach(function(_synonymousList, _id) {
+
+      if (!visitedTable[_id]) {
+        var synonymousSet = [];
+        var stack = [];
+        visitedTable[_id] = true;
+        synonymousSet.push(_id);
+        stack.push(_id);
+        while (stack.length) {
+          var id = stack.pop();
+          var synonymousList = synonymousIdTable[id];
+          for (var i = 0, length = synonymousList.length; i < length; i++) {
+            var synonymousId = synonymousList[i];
+            if (!synonymousId) continue;
+            if (!visitedTable[synonymousId]) {
+              visitedTable[synonymousId] = true;
+              synonymousSet.push(synonymousId);
+              stack.push(synonymousId);
+            }
+          }
+        }
+
+        var trueId = Math.min.apply(Math, synonymousSet);
+        synonymousSet.forEach(function(id) {
+          lookupTable[id] = trueId;
+        });
+      }
+
+    });
+
+    for (var i = 1; i < nextMarkId; i++) {
+      lookupTable[i] = lookupTable[i] || i;
+    }
+    return lookupTable;
+  })();
+
+  (function _mergeGroups() {
+    for (var iy = 0; iy < height; iy++) {
+      var currentRow = groupTable[iy];
+      for (var ix = 0; ix < width; ix++) {
+        currentRow[ix] = lookupTable[currentRow[ix]];
+      }
+    }
+  })();
+
+  var groupIdList = (function _createGroupIdList() {
+    var uniqueSparseArray = [];
+    var
+      length,
+      id,
+      i;
+    for (i = 0, length = lookupTable.length; i < length; i++) {
+      id = lookupTable[i];
+      if (id) uniqueSparseArray[id] = id;
+    }
+
+    var uniqueArray = [];
+    for (i = 0, length = uniqueSparseArray.length; i < length; i++) {
+      id = uniqueSparseArray[i];
+      if (id) uniqueArray.push(id);
+    }
+
+    return uniqueArray;
+  })();
+
+  return {
+    groupTable: groupTable,
+    groupIdList: groupIdList
+  };
+}
